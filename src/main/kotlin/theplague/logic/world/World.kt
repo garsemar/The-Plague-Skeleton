ef09618f320c795311a285023117943b21aa3b77
garsemar <martigarciaserra@gmail.com>
2022-05-08 23:53:30 +0200

    Documentacion

--- src/main/kotlin/theplague/logic/world/World.kt
+++ src/main/kotlin/theplague/logic/world/World.kt
@@ -37,10 +37,16 @@ class World(
         this.player.turns++;
     }
 
+    /**
+     * Function that returns a random position on the map.
+     */
     private fun getRandomWorldPosition() : Position {
         return Position((0 until width).random(), (0 until height).random())
     }
 
+    /**
+     * This function is called at the end of each turn.
+     */
     override fun nextTurn() {
 
         if(player.currentVehicle.timesLeft == 0)
@@ -54,15 +60,24 @@ class World(
         expandPlague()
     }
 
+    /**
+     * This function calls another function from the position of the list you pass it.
+     */
     private fun generateNewColonies() {
         val colonyPos = getRandomWorldPosition();
         territories[colonyPos.x][colonyPos.y].summonPlague()
     }
 
+    /**
+     * This function checks that the player is not in the territory and calls the reproducePlague function.
+     */
     private fun reproducePlague() {
         territories.flatten().forEach { if(it.position.x != player.position.x && it.position.x != player.position.y) it.reproducePlague(Position(width, height)) }
     }
 
+    /**
+     * This function calls the expand function passing as parameter the class Position and the maxPosition.
+     */
     private fun expandPlague() {
         territories.flatten().forEach {
             val colony = it.plague;
@@ -70,19 +85,31 @@ class World(
         }
     }
 
+    /**
+     * This function calls for each territory the function spawnItem.
+     */
     private fun generateNewItems() {
         val itemPos = getRandomWorldPosition();
         territories[itemPos.x][itemPos.y].spawnItem()
     }
 
+    /**
+     * This function returns true if the game ends.
+     */
     override fun gameFinished(): Boolean {
         return false
     }
 
+    /**
+     * This function checks if the position the player wants to move to is possible.
+     */
     override fun canMoveTo(targetPos: Position) : Boolean{
         return player.currentVehicle.canMove(player.position, targetPos)
     }
 
+    /**
+     * This function moves the player to the position that is passed as a parameter.
+     */
     override fun moveTo(nextPosition: Position) {
         // Remove player from territory
         actualPlayerTerritory.removePlayer()
@@ -99,14 +126,23 @@ class World(
         player.turns++;
     }
 
+    /**
+     * This function calls the function attackPlague.
+     */
     override fun exterminate() {
         actualPlayerTerritory.attackPlague()
     }
 
+    /**
+     * This function calls the function that is in charge of printing the item that the user is going to take.
+     */
     override fun takeableItem(): Iconizable? {
         return takeableItem
     }
 
+    /**
+     * This function checks if the item is a weapon or a vehicle and assigns the one taken by the user.
+     */
     override fun takeItem() {
         if(takeableItem != null) {
             when(takeableItem) {
